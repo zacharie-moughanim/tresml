e2ab7a82e6408139c73f288d1437cc81d15a6482
ZachMGH <zachariemoughanim@gmail.com>
2026-03-05 19:01:54 +0100

    Implement basic syntax highlighting and bracket matching

--- src/extension.ts
+++ src/extension.ts
@@ -8,18 +8,18 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tresml" is now active!');
+	console.log('tresml is now active');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('tresml.unesuperfonction', () => {
+	// const disposable = vscode.commands.registerCommand('tresml.foo', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('AHHHHHHHHHHHHHFDSJKFJSLDJFSDKJFLSDKJFKL');
-	});
+		// vscode.window.showInformationMessage('foo was just run');
+	// });
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
